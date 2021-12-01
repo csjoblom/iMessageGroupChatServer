2b5e3337c94ac708995053436ef363a895991881
Christopher Sjoblom <azulle@Christophers-iMac.local>
2021-11-30 17:30:45 -0800

    api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "imessagechatgroup": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
